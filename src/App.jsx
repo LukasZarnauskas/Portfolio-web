0e9a0f05ac21735d83c65fbd2a40e0cdfb9950b7
GluteusMedius <zarnauskas97@gmail.com>
2023-06-10 13:28:22 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import './styles/reset.css';
+import './styles/App.css';
+
+function App() {
+  return <div className="container"></div>;
+}
+export default App;
---
+++ src/components/ui/layout/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './header.scss';
+function Header() {
+  return <div>Header</div>;
+}
+
+export default Header;
