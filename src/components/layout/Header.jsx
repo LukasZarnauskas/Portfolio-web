7a76391396d7b65c0e9606fb1da4d0a615e4312b
GluteusMedius <zarnauskas97@gmail.com>
2023-06-10 18:51:58 +0300

    header style

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -7,13 +7,47 @@ const StyledHeader = styled.header`
   color: #fff;
   background-color: #000;
 `;
-const Logo = styled.a``;
-const Nav = styled.nav``;
-const NavItem = styled.a``;
+const Logo = styled.a`
+  font-size: 2rem;
+  font-weight: 700;
+  &:hover {
+    background-color: #fff;
+    color: #000;
+    padding-right: 0.5rem;
+    transition: 0.5s;
+  }
+  &:hover > span {
+    background-color: #000;
+    color: #fff;
+    transition: 0.5s;
+  }
+`;
+
+const LogoFirst = styled.span`
+  background-color: #fff;
+  color: #000;
+  padding: 0rem 0.5rem;
+`;
+const Nav = styled.nav`
+  display: flex;
+  gap: 1.5rem;
+`;
+const NavItem = styled.a`
+  font-size: 1.8rem;
+  padding: 0 0.5rem;
+  &:hover {
+    background-color: #fff;
+    color: #000;
+    transition: 0.5s;
+    border-radius: 0.5rem;
+  }
+`;
 function Header() {
   return (
     <StyledHeader>
-      <Logo href="/">LUKAS PORTFOLIO</Logo>
+      <Logo href="/">
+        <LogoFirst>LUKAS</LogoFirst> PORTFOLIO
+      </Logo>
       <Nav>
         <NavItem href="/">Home</NavItem>
         <NavItem href="/">About</NavItem>
