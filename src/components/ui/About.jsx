3cdb5554eea4c5be8ecaec75d7325f92bd5dfada
GluteusMedius <zarnauskas97@gmail.com>
2023-06-18 19:12:40 +0300

    See work added, work experience and style update

--- src/components/ui/About.jsx
+++ src/components/ui/About.jsx
@@ -2,21 +2,19 @@
 import styled from "styled-components";
 const StyledAbout = styled.section`
   display: flex;
-  align-items: center;
   flex-direction: column;
-  padding: 10rem 0;
+  padding: 10rem 20rem;
   background-color: #39b54a;
-  height: 100vh;
 `;
 const AboutH2 = styled.h2`
   font-size: 5rem;
+  text-align: center;
 `;
 const P = styled.p`
   font-size: 2rem;
   margin-top: 5rem;
   color: whitesmoke;
   text-align: center;
-  padding: 0 25rem;
 `;
 const Button = styled.button`
   background-color: initial;
@@ -35,35 +33,62 @@ const Button = styled.button`
 const Div = styled.div`
   display: flex;
   flex-direction: column;
+  margin-top: 5rem;
+  border-right: 3px solid whitesmoke;
+  padding-right: 5rem;
+`;
+const DivW = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 5rem;
+  margin-left: 5rem;
+`;
+const DivHist = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const DivWorks = styled.div`
+  display: flex;
+  gap: 5rem;
 `;
 const H3 = styled.h3`
   font-size: 3.5rem;
+  margin-top: 5rem;
+`;
+const H4 = styled.h4`
+  margin-top: 2rem;
 `;
 const Ul = styled.ul`
   list-style: none;
   color: whitesmoke;
+  padding-left: 0;
 `;
 const Li = styled.li`
-  display: flex;
+  margin-top: 0.5rem;
+  font-size: 2rem;
 `;
 const LiEd = styled.li`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  font-size: 2rem;
 `;
 
 const Img = styled.img`
   width: 2rem;
+  margin-right: 0.5rem;
+  vertical-align: top;
 `;
-const H4 = styled.h4``;
-const PEd = styled.p``;
-const Year = styled.p``;
+
+const PEd = styled.p`
+  margin-top: 0.5rem;
+`;
+const Year = styled.p`
+  margin-top: 0.5rem;
+`;
 function About() {
   return (
     <StyledAbout>
       <AboutH2>ABOUT MY</AboutH2>
-
       <P>
         I have gained experience in the field of health care by working as
         physiotherapist. The time has come when I decided to change mine
@@ -71,45 +96,97 @@ function About() {
         why I'm currently looking for a job where I can develop as a frontend
         developer.
       </P>
-      <Div>
-        <H3>My skills</H3>
-        <Ul>
-          <Li>
-            <Img src="/src/images/html.png" alt="html" /> HTML
-          </Li>
-          <Li>
-            <Img src="/src/images/css-3.png" alt="html" />
-            CSS
-          </Li>
-          <Li>
-            <Img src="/src/images/java-script.png" alt="html" />
-            JavaScript
-          </Li>
-          <Li>
-            <Img src="/src/images/atom.png" alt="html" />
-            React
-          </Li>
-        </Ul>
-        <H3>Education</H3>
-        <Ul>
-          <LiEd>
-            <H4>Physiotherapy Bachelor</H4>
-            <PEd>Lithuania Sport University</PEd>
-            <Year>2016 - 2020</Year>
-          </LiEd>
-          <LiEd>
-            <H4>Massager qualification</H4>
-            <PEd>Karalius Mindaugas Vocational training centre</PEd>
-            <Year>2020</Year>
-          </LiEd>
-          <LiEd>
-            <H4>Frontend developer</H4>
-            <PEd>CodeAcademy</PEd>
-            <Year>560 hours</Year>
-          </LiEd>
-        </Ul>
-      </Div>
-      <Button> MORE ABOUT MY</Button>
+      <DivHist>
+        <Div>
+          <H3>My skills</H3>
+          <Ul>
+            <Li>
+              <Img src="/src/images/html.png" alt="html" />
+              HTML
+            </Li>
+            <Li>
+              <Img src="/src/images/css-3.png" alt="html" />
+              CSS
+            </Li>
+            <Li>
+              <Img src="/src/images/java-script.png" alt="html" />
+              JavaScript
+            </Li>
+            <Li>
+              <Img src="/src/images/atom.png" alt="html" />
+              React
+            </Li>
+          </Ul>
+
+          <H3>Education</H3>
+          <Ul>
+            <div>
+              <LiEd>
+                <H4>Physiotherapy Bachelor</H4>
+                <PEd>Lithuania Sport University</PEd>
+                <Year>2016 - 2020</Year>
+              </LiEd>
+            </div>
+            <div>
+              <LiEd>
+                <H4>Massager qualification</H4>
+                <PEd>Karalius Mindaugas Vocational training centre</PEd>
+                <Year>2020</Year>
+              </LiEd>
+            </div>
+            <div>
+              <LiEd>
+                <H4>Frontend developer</H4>
+                <PEd>CodeAcademy</PEd>
+                <Year>560 hours</Year>
+              </LiEd>
+            </div>
+          </Ul>
+        </Div>
+        <DivW>
+          <H3>Work experience</H3>
+          <Ul>
+            <div>
+              <LiEd>
+                <H4>Physiotherapist</H4>
+                <PEd>VšĮ „Pagrindinis ortopedijos servisas”</PEd>
+                <Year>2019-02-11 2019-07-31</Year>
+              </LiEd>
+            </div>
+            <div>
+              <LiEd>
+                <H4>Physiotherapist</H4>
+                <PEd>Šakių 'Vytis' basketball team</PEd>
+                <Year>2019/09 - 2021/04</Year>
+              </LiEd>
+            </div>
+            <div>
+              <LiEd>
+                <H4>Physiotherapist</H4>
+                <PEd>Lithuania Beach football national team</PEd>
+                <Year>2021/07</Year>
+              </LiEd>
+            </div>
+            <div>
+              <LiEd>
+                <H4>Physiotherapist</H4>
+                <PEd>Utenos 'Juventus' basketball team</PEd>
+                <Year>2021/08 - 2022</Year>
+              </LiEd>
+            </div>
+            <div>
+              <LiEd>
+                <H4>Physiotherapist</H4>
+                <PEd>Uab 'Slauga ir Priežiūra'</PEd>
+                <Year>2022-2023</Year>
+              </LiEd>
+            </div>
+          </Ul>
+        </DivW>
+      </DivHist>
+      <DivWorks>
+        <H3>You can see my works here:</H3> <Button>Works</Button>
+      </DivWorks>
     </StyledAbout>
   );
 }
